7b9179aa8ab9a810da9e0614b7948b462d2fe67e
Danny Fonseca <daniela.fonsecazumbado@gmail.com>
2024-06-10 22:11:13 -0600

    Test: Modified ui_test.c to include a window box with the basic structure the UI will have, excluding the menu

--- src/ui_test.c
+++ src/ui_test.c
@@ -6,20 +6,31 @@ int main(int argc, char ** argv){
     // Configura memoria y limpia la pantalla
     initscr();
 
-    // Posicion del cursor
-    int x, y;
-    x = y = 5;
+    // Posicion de la ventana
+    int height, width, start_y, start_x;
+    height = 20;
+    width = 70;
+    start_y = 2;
+    start_x = 4;
 
-    // Mueve el cursor a una ubicacion especificada
-    move(y, x);
-    
-    // Imprime una string en la ventana
-    printw("Hola mundo");
-
-    // Refresca la pantalla para que aparezca lo que esta en memoria
+    // Crea una ventana
+    WINDOW * win = newwin(height, width, start_y, start_x);
     refresh();
 
+    // Crea bordes alrededor de la ventana
+    box(win, 0, 0);
+    mvwprintw(win, 2, 4, "Riddle Game\n");
+    mvwprintw(win, 4, 4, "Success:\n");
+    mvwprintw(win, 5, 4, "timer placeholder\n");
+    mvwprintw(win, 7, 4, "QX: Question placeholder\n");
+    mvwprintw(win, 9, 4, "A) Option A\n");
+    mvwprintw(win, 10, 4, "B) Option B\n");
+    mvwprintw(win, 11, 4, "C) Option C\n");
+    mvwprintw(win, 12, 4, "D) Option D\n");
+    wrefresh(win);
+
     // Retorna el valor int del key del user input
+    getch();
     getch();
 
     // Libera la memoria y cierra ncurses
