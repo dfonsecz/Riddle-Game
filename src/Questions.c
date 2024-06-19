c1bb4aeafe2df0ea77ba3dbd4d9279b2b2434a53
Fabian <fabian@Ubuntu.myguest.virtualbox.org>
2024-06-18 20:16:08 -0600

    draft/ add the order to add the new questions accurate the structure.

--- src/Questions.c
+++ src/Questions.c
@@ -25,12 +25,6 @@ void leer_preguntas(const char *nombre_archivo) {
 }
 
 
-int main() {
-    const char *nombre_archivo = "preguntas.txt";
-     leer_preguntas(nombre_archivo);
-}
-
-
 void agregarNuevaPregunta(const char *nombre_archivo){
      FILE *archivo = fopen(nombre_archivo, "a");
     if (archivo == NULL) {
@@ -38,4 +32,20 @@ void agregarNuevaPregunta(const char *nombre_archivo){
         return;
     }
 
+    Pregunta Nuevapregunta;
+
+    printf("ingrese la categoria");// pedirle al usuario la categoria
+    fgets(Nuevapregunta.categoria, sizeof(Nuevapregunta.categoria), stdin);// leemos la pregunta con fgets desde la entrada estandar, sizeof para especificar el tama;o de liínea de entrada, prevenimos desbordamientos de buffer.
+    Nuevapregunta.categoria[strcspn(Nuevapregunta.categoria, "\n")] = '\0'; // Eliminamos el caracter de nueva linea con strcspn
+
 }
+
+int main() {
+    const char *nombre_archivo = "preguntas.txt";
+     leer_preguntas(nombre_archivo);
+
+     agregarNuevaPregunta(nombre_archivo);
+}
+
+
+
