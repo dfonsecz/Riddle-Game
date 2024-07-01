5dcfb15acce7d9af03742d7c26cbb13e9e750b57
Danny Fonseca <daniela.fonsecazumbado@gmail.com>
2024-06-30 23:21:29 -0600

    Test: Expanded menu.h

--- src/menu.h
+++ src/menu.h
@@ -1,8 +1,7 @@
-#include <curses.h>
-#include <string.h>
-
 #ifndef _MENU_H_
 #define _MENU_H_
+
+#include <ncurses.h>
 
 typedef struct {
     int start_x;
@@ -15,5 +14,13 @@ typedef struct {
     Menu *menus;
     int num_menus;
 } MenuBar;
+
+typedef struct {
+    char **options;
+    int num_options;
+} SubMenu;
+
+// Declaración de submenuWin
+char *submenuWin(WINDOW *parent_win, SubMenu submenu);
 
 #endif
