78a39ebd275de424d40a6ba08c7ce0f591689a36
Danny Fonseca <daniela.fonsecazumbado@gmail.com>
2024-07-01 21:06:26 -0600

    Modified main.c for a more modular program

--- src/main.c
+++ src/main.c
@@ -1,23 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <ncurses.h>
-#include "../include/menu.h" // Incluye el archivo de cabecera que contiene la declaración de menuWin
+#include "../include/menu.h"
 
 int main() {
-    // Inicializa ncurses
     initscr();
     noecho();
-    curs_set(0);
-
-    // Llama a la función para manejar el menú
-    WINDOW *menu = menuWin();
-
-    // Espera a la entrada del usuario
-    getch();
-
-    // Limpia y cierra ncurses
-    delwin(menu);
+    cbreak();
+    
+    // Start the main menu
+    menuWin();
+    
+    // Clean up
     endwin();
-
     return 0;
 }
