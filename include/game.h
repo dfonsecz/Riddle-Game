ea9269f027ac408d42eb10d50c7aaad6187972c6
Danny Fonseca <daniela.fonsecazumbado@gmail.com>
2024-07-05 00:22:35 -0600

    Edited header file game.h to include additional declarations of functions

--- include/game.h
+++ include/game.h
@@ -3,6 +3,21 @@
 
 #include <ncurses.h>
 
-int openGame(WINDOW *win, char *category);
+#define MAX_PREGUNTAS 100
+
+// Estructura para almacenar preguntas
+typedef struct {
+    char categoria[50];
+    char pregunta[256];
+    char opciones[4][50];
+    int respuestaCorrecta; // Índice correcto
+} Pregunta;
+
+int leerPreguntasDesdeArchivo(const char *nombreArchivo, Pregunta preguntas[], int maxPreguntas);
+Pregunta *seleccionarPreguntaAleatoria(Pregunta preguntas[], int numPreguntas, char *category);
+void mostrarPregunta(WINDOW *win, Pregunta *p);
+int obtenerRespuestaUsuario(WINDOW *win);
+
+void openGame(WINDOW *win, char *category);
 
 #endif
